f2e3d4a2428c4c3c605b6f0c3e261c6e62a812a8
TSxSAHIL <tssahil2001@gmail.com>
2023-04-29 09:13:54 +0530

    new

--- lib/login/signup/LoginApi.dart
+++ lib/login/signup/LoginApi.dart
@@ -1,7 +1,6 @@
 import 'dart:convert';
 
 import 'package:http/http.dart' as http;
-
 class LoginApi {
   static Future<String?> login(String username, String password) async {
     const baseUrl = 'https://example.com/api';
