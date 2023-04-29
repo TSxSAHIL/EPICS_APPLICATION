f327a21839fe49a9ca16b35e0749a02edfa5d1dd
TSxSAHIL <tssahil2001@gmail.com>
2023-04-29 09:14:18 +0530

    new

--- lib/screens/booking_page.dart
+++ lib/screens/booking_page.dart
@@ -9,8 +9,7 @@ import '../components/customized_app_bar.dart';
 
 class BookingPage extends StatefulWidget {
   const BookingPage({super.key});
-  
-  @override
+    @override
   State<BookingPage> createState() => _BookingPageState();
 }
 class _BookingPageState extends State<BookingPage> {
