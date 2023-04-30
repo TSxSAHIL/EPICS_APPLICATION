8b895cfcada0dee45320091c95ddcf6d92f487ec
TSxSAHIL <tssahil2001@gmail.com>
2023-04-30 09:11:10 +0530

    new

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,7 @@ import 'package:epics_project/screens/success.dart';
 import 'package:epics_project/utils/config.dart';
 import 'package:flutter/material.dart';
 import 'login/login.dart';
+
 void main() {
   runApp(MaterialApp(
     theme: ThemeData(
